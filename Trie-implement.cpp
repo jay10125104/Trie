4bd8e9ba00de2f1f35def88069354444fd75b165
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2022-09-20 17:56:10 +0530

    Create Trie-implement.cpp
    trie basic implementation

---
+++ Trie-implement.cpp
@@ -0,0 +1,48 @@
+struct node{
+    node*links[26];
+    bool flag=false;
+};
+class Trie {
+public:
+    node*root;
+    Trie() {
+        root = new node();
+    }
+    
+    void insert(string s) {
+        node*temp = root;
+        for(int i=0;i<s.size();i++){
+            if(temp->links[s[i]-'a']==NULL){
+                temp->links[s[i]-'a'] =  new node();
+            }
+            temp = temp->links[s[i]-'a'];
+        }
+        temp->flag = true;
+    }
+    
+    bool search(string s) {
+        node*temp = root;
+        for(int i=0;i<s.size();i++){
+            if(temp->links[s[i]-'a']==NULL){
+                return false;
+            }
+            else{
+                temp = temp->links[s[i]-'a'];
+            }
+        }
+        return temp->flag;
+    }
+    
+    bool startsWith(string s) {
+        node*temp = root;
+        for(int i=0;i<s.size();i++){
+            if(temp->links[s[i]-'a']==NULL){
+                return false;
+            }
+            else{
+                temp = temp->links[s[i]-'a'];
+            }
+        }
+        return true;
+    }
+};
